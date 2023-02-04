8a2ebb7d08ba91364159f27ab2bc74950d0d6c00
hyperFounder <RXR274@student.bham.ac.uk>
2023-02-04 18:34:54 +0000

    Exit button method working

--- src/main/java/com/example/mytictactoe/HelloController.java
+++ src/main/java/com/example/mytictactoe/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.mytictactoe;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void ExitApp()
+    {
+        System.exit(0);
     }
+
 }
